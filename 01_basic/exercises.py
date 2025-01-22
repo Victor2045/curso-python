19e3384f77f5d9a05bcfa595334e92852f6e0909
Victor2045 <victorgeneral.vk@gmail.com>
2025-01-22 10:39:21 -0400

    Tarea completa de la clase anterior

--- 01_basic/exercises.py
+++ 01_basic/exercises.py
@@ -8,7 +8,7 @@ print("Escribe un programa que imprima tu nombre y tu ciudad en líneas separada
 
 ### Completa aquí
 
-print("--------------")
+print("Victor Presilla", "Upata", sep="\n")
 
 print("\nEjercicio 2: Muestra los tipos de datos de las siguientes variables:")
 print("Usa el comando 'type()' para determinar el tipo de datos de cada variable.")
@@ -20,15 +20,24 @@ e = None
 
 ### Completa aquí
 
-print("--------------")
+print(f"La variable a es de tipo: {type(a)}")
+print(f"La variable b es de tipo: {type(b)}")
+print(f"La variable c es de tipo: {type(c)}")
+print(f"La variable d es de tipo: {type(d)}")
+print(f"La variable e es de tipo: {type(e)}")
 
 print("\nEjercicio 3: Casting de tipos")
 print("Convierte la cadena \"12345\" a un entero y luego a un float.")
 print("Convierte el float 3.99 a un entero. ¿Qué ocurre?")
 
 ### Completa aquí
+cadena = "12345"
+numero = 3.99
 
-print("--------------")
+print(int(cadena))
+print(float(cadena))
+print(int(numero))
+print("La funcion int() se encarga de eliminar los digitos despues del punto, no como la funcion round() que se encarga de redondear un numero determinado por sus decimales")
 
 print("\nEjercicio 4: Variables")
 print("Crea variables para tu nombre, edad y altura.")
@@ -37,11 +46,20 @@ print("Usa f-strings para imprimir una presentación.")
 # "Hola! Me llamo midudev y tengo 39 años, mido 1.70 metros"
 
 ### Completa aquí
+name = "Victor"
+edad = 25
+altura = 1.83
 
-print("--------------")
+print(f"Hola! Me llamo {name} y tengo {edad} años, mido {altura} metros")
 
 print("\nEjercicio 5: Números")
 print("1. Crea una variable con el número PI (sin asignar una variable)")
 print("2. Redondea el número con round()")
 print("3. Haz la división entera entre el número que te salió y el número 2")
 print("4. El resultado debería ser 1")
+
+numero = round(3,14159)
+
+numero = int(numero / 2)
+
+print(f"El resultado es: {numero}")
